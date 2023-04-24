72ccfd971f55ccf2c0911d5cfea9d0863f3de4e8
unknown <sumitseems@gmail.com>
2023-04-24 19:14:22 -0400

    added demo lambda code

--- lambda_function.py
+++ lambda_function.py
@@ -15,11 +15,31 @@ clusterendpoint = os.environ.get("docdbEndpoint")
 def lambda_handler(event, context):
     ##########################################New Doc DB Code###############
     client = pymongo.MongoClient(clusterendpoint, username=username, password=password, tls='true', tlsCAFile='rds-combined-ca-bundle.pem',retryWrites='false')
+    
+    ##Create New DB
     db = client.mynew_database
+    
+    ##New Collection for DB
+    
+    col = db.mynewnew_democollection
+    
+    ##Insert a single document
+    col.insert_one({'hello':'Amazon DocumentDB'})
+    
+    ##Find the document that was previously written
+    x = col.find_one({'hello':'Amazon DocumentDB'}) 
+    
+    ##Print the result to the screen
+    print(x)
     print(client.list_database_names())
     print(db.list_collection_names())
+    
+    ##Drop Collection 
+    db.drop_collection('sample_collection')
+    
     ###########################################old code##########################
+      
     d = {'col1': [1,2], 'col2': [3,4]}
     df = pd.DataFrame(data=d)
     print(df)
-    print('Done x1.1.1.1')
+    print('Done x1.1.1.2')
