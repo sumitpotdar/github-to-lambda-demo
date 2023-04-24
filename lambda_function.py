4656909bfca7c356425bc5806f558b231e3f7fc4
unknown <sumitseems@gmail.com>
2023-04-24 18:54:41 -0400

    added demo lambda code

--- lambda_function.py
+++ lambda_function.py
@@ -7,7 +7,17 @@ import boto3
 import base64
 import botocore
 
+#########################New Update#################
+username = os.environ.get("docdbUser")
+password = os.environ.get("docdbPass")
+clusterendpoint = os.environ.get("docdbEndpoint")
+
 def lambda_handler(event, context):
+    ##########################################New Doc DB Code###############
+    client = pymongo.MongoClient(clusterendpoint, username=username, password=password, tls='true', tlsCAFile='rds-combined-ca-bundle.pem',retryWrites='false')
+    print(client.list_database_names())
+    print(db.list_collection_names())
+    ###########################################old code##########################
     d = {'col1': [1,2], 'col2': [3,4]}
     df = pd.DataFrame(data=d)
     print(df)
