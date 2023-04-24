a4fe76e5bb47ba9abd6a97c4215b6c73531e5fc4
unknown <sumitseems@gmail.com>
2023-04-24 17:58:56 -0400

    added demo lambda code

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import pymongo
+import os
+import sys
 
 def lambda_handler(event, context):
     d = {'col1': [1,2], 'col2': [3,4]}
     df = pd.DataFrame(data=d)
     print(df)
-    print('Done x1.1')
+    print('Done x1.1.1')
