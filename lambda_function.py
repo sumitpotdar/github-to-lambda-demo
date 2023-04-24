d79720fc50f5cc98ea986dbf487f57f3a75c5758
unknown <sumitseems@gmail.com>
2023-04-24 18:38:55 -0400

    added demo lambda code

--- lambda_function.py
+++ lambda_function.py
@@ -2,9 +2,13 @@ import pandas as pd
 import pymongo
 import os
 import sys
+import json
+import boto3
+import base64
+import botocore
 
 def lambda_handler(event, context):
     d = {'col1': [1,2], 'col2': [3,4]}
     df = pd.DataFrame(data=d)
     print(df)
-    print('Done x1.1.1')
+    print('Done x1.1.1.1')
